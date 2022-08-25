43728afa38d0187150da5b080369c190c6627c52
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-25 23:04:22 +0430

    feat: add experience buckets

---
+++ experience_buckets.py
@@ -0,0 +1,16 @@
+def init_experiences():
+    EXPERIENCES = [
+        "mobile developer",  # 0
+        "data scientist",  # 1
+        "ui-ux designer",  # 2
+        "network engineer",  # 3
+        "data engineer",  # 4
+        "software engineer",  # 5
+        "full stack developer",  # 6
+        "frontend developer",  # 7
+        "hardware engineer",  # 8
+        "devops engineer",  # 9
+        "database administrator",  # 10
+    ]
+
+    return EXPERIENCES
