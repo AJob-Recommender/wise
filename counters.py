ea2ff0edd45305b08fed91a619f831dfc298402c
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-25 23:03:56 +0430

    feat: add initial counters

---
+++ counters.py
@@ -0,0 +1,41 @@
+def init_counters():
+    # counter for skills
+    counters = {
+        "name": "",
+        "counter_skills_programming": 0,
+        "counter_skills_backend": 0,
+        "counter_skills_devops": 0,
+        "counter_skills_frontend": 0,
+        "counter_skills_system_design": 0,
+        "counter_skills_version_control": 0,
+        "counter_skills_ui_ux": 0,
+        "counter_skills_data_related": 0,
+        "counter_skills_databases": 0,
+        "counter_skills_ai_modeling": 0,
+        "counter_skills_ai_frameworks": 0,
+        "counter_skills_network": 0,
+        "counter_skills_message_brokers": 0,
+        "counter_skills_hardware": 0,
+        "counter_skills_os": 0,
+        # counter for educations
+        "counter_educations_bachelor": 0,
+        "counter_educations_master": 0,
+        "counter_educations_phd": 0,
+        "counter_educations_other_majors": 0,
+        # counter for experiences
+        "counter_mobile_developer": 0,
+        "counter_data_scientist": 0,
+        "counter_ui_ux_designer": 0,
+        "counter_network_engineer": 0,
+        "counter_data_engineer": 0,
+        "counter_software_engineer": 0,
+        "counter_fullstack_developer": 0,
+        "counter_frontend_developer": 0,
+        "counter_hardware_engineer": 0,
+        "counter_devops_engineer": 0,
+        "counter_database_administrator": 0,
+        "counter_other_experiences": 0,
+        "current_job": ""
+    }
+
+    return counters
