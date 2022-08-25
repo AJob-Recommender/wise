a0484bd68b86a529520b5f5de873e85d9812a6a9
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-25 23:03:46 +0430

    feat: add item constructor

---
+++ Items.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+
+
+class Items(BaseModel):
+    programming_languages = {}
+    backend_frameworks = {}
+    devops_technologies = {}
+    frontend_frameworks = {}
+    system_design_skills = {}
+    version_controls = {}
+    ui_ux = {}
+    data_related = {}
+    databases = {}
+    ai_models = {}
+    ai_frameworks = {}
+    network_skills = {}
+    message_brokers = {}
+    hardware_skills = {}
+    os = {}
+    # Education
+    bachelor_computer_engineering = {}
+    master_computer_engineering = {}
+    phd_computer_engineering = {}
+    other_majors = {}
+    # Experiences
+    mobile_developers = {}
+    data_scientists = {}
+    ui_ux_designer = {}
+    network_engineer = {}
+    data_engineer = {}
+    software_engineer = {}
+    fullstack_developer = {}
+    frontend_developer = {}
+    hardware_engineer = {}
+    devops_engineer = {}
+    database_administrator = {}
+
+
+
