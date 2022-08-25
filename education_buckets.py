239ca4413ceeadebaeb8c78af4988b7e9c8b8b0b
Amir Hallaji <a.hallaji.b@gmail.com>
2022-08-25 23:04:13 +0430

    feat: add education buckets

---
+++ education_buckets.py
@@ -0,0 +1,10 @@
+def init_educations():
+    EDUCATIONS = [
+        "bachelor-computer engineering",
+        "master-computer engineering",
+        "phd-computer engineering",
+        "others",
+    ]
+
+
+    return EDUCATIONS
